ee985e34fe73208b79aaa1da8db3c53c93fe5b5a
Yiyang Lu <ste900811@gmail.com>
2025-05-08 15:54:17 -0400

    Adding AMEX Hilton Honors Aspire Card for amy and howard

---
+++ README.md
@@ -0,0 +1,13 @@
+Currently only run on the Windows machine, using below example terminal code to run the main file:
+python main.py discover cashBack stevenLu 250216
+  discover = name of the bank
+  cashBack = credit card type
+  stevenLu = credit card holder
+  250216 = date of the creaditcard statment
+
+expensesHanddler Folder is holding all the expenses parsing places
+mainFunctions Folder is the function using in the main.py
+objects Folder is the place holding all the calsses
+  banks = banks such as discover, capitalOne, etc
+  people = group by the household, because different household will has different way to extract and output data
+statements Folder is the place holding all the credit card statement, it then group by card holder name
--- mainFunctions/createObjects.py
+++ mainFunctions/createObjects.py
@@ -15,12 +15,17 @@ def createObjects(bankName, cardType, statementDate, personName):
   elif bankName == "capitalOne" and cardType == "ventureX" and personName == "maridaCheng":
     bankObj = CapitalOne(bankName, cardType, statementDate)
     personObj = stevenLuHome()
-  elif bankName == "AMEX" and cardType == "businessGoldCard" and personName == "maridaCheng":
+  elif bankName == "AMEX" and cardType == "BusinessGoldCard" and personName == "maridaCheng":
+    bankObj = AMEX(bankName, cardType, statementDate)
+    personObj = amyLuHome()
+  elif bankName == "AMEX" and cardType == "HiltonHonorsAspireCard" and personName == "amyLu":
+    bankObj = AMEX(bankName, cardType, statementDate)
+    personObj = amyLuHome()
+  elif bankName == "AMEX" and cardType == "HiltonHonorsAspireCard" and personName == "howardYao":
     bankObj = AMEX(bankName, cardType, statementDate)
     personObj = amyLuHome()
   else:
     print(f'createObject failed: Bank: {bankName}, Card Type: {cardType}, Person: {personName}')
     sys.exit(1)
-
 
   return bankObj, personObj
